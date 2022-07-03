37b1544566a5a69912fe00ca9a49ac3de76cbc25
Huakun Shen <huakun.shen@huakunshen.com>
2022-07-03 12:22:53 -0400

    Update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,24 @@
 export default {
-    title: 'CrossCopy',
-    description: 'CrossCopy Documentation'
-}
+  title: "CrossCopy",
+  description: "CrossCopy Documentation",
+  themeConfig: {
+    editLink: {
+      pattern: 'https://github.com/CrossCopy/crosscopy-doc/edit/main/docs/:path'
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/CrossCopy/crosscopy-doc" },
+      { icon: "twitter", link: "https://twitter.com/crosscopyio" },
+    ],
+    sidebar: {
+      "/introduction/": [
+        {
+          text: "Introduction",
+          items: [
+            { text: "Introduction", link: "/introduction/index" },
+            { text: "Getting Started", link: "/introduction/getting-started" },
+          ],
+        },
+      ],
+    },
+  },
+};
