52b948256517ec34a4b9e4df3d9ec85164404d85
Huakun Shen <huakun.shen@huakunshen.com>
2022-08-20 01:10:29 -0400

    Implemented contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,102 +1,141 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useSnackbar } from "notistack";
+import { Button, TextField } from "@mui/material";
 
 const ContactForm = () => {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
+  const { enqueueSnackbar } = useSnackbar();
+  const [inputErr, setInputErr] = useState({
+    first: false,
+    last: false,
+    email: false,
+    msg: false,
+  });
+  const [inputErrMsg, setInputErrMsg] = useState({
+    first: "",
+    last: "",
+    email: "",
+    msg: "",
+  });
+
   const submit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    fetch(
-      "https://notification-proxy.crosscopy.io/notion",
-      {
-        method: "POST",
-        mode: "no-cors",
-        body: JSON.stringify({
-          message: msg,
-          email,
-          name: `${firstname} ${lastname}`,
-          tags: ["CrossCopy", "Doc"],
-        }),
-      }
-    );
-  };
+    let err = false;
+    let msgs = {
+      first: "",
+      last: "",
+      email: "",
+      msg: "",
+    };
+    if (firstname === "") {
+      msgs.first = "Please enter your first name";
+      err = true;
+    }
+    if (lastname === "") {
+      msgs.last = "Please enter your last name";
+      err = true;
+    }
+    if (email === "") {
+      msgs.email = "Please enter your email name";
+      err = true;
+    }
+    if (msg === "") {
+      msgs.msg = "Message cannot be empty";
+      err = true;
+    }
+    const errorIndicators = {
+      first: Boolean(msgs.first),
+      last: Boolean(msgs.last),
+      email: Boolean(msgs.email),
+      msg: Boolean(msgs.msg),
+    };
 
+    setInputErr(errorIndicators);
+    if (err) {
+      setInputErrMsg(msgs);
+      return;
+    }
+    fetch("https://notification-proxy.crosscopy.io/notion", {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify({
+        message: msg,
+        email,
+        name: `${firstname} ${lastname}`,
+        tags: ["CrossCopy", "Doc"],
+      }),
+    })
+      .then(() => {
+        enqueueSnackbar("Message Sent", { variant: "success" });
+        setFirstname("");
+        setLastname("");
+        setEmail("");
+        setMsg("");
+      })
+      .catch((error) => {
+        console.error(error);
+        enqueueSnackbar("Failed to Send Message", { variant: "error" });
+      });
+  };
   return (
-    <form className="w-full max-w-lg mx-auto" onSubmit={submit}>
-      <div className="flex justify-center">
-        <h1 className="text-5xl">Contact Us</h1>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-            htmlFor="grid-first-name"
-          >
-            First Name
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-first-name"
-            type="text"
-            placeholder="First"
+    <section className="bg-white py-5">
+      <form className="w-full max-w-lg mx-auto" onSubmit={submit}>
+        <div className="flex justify-center">
+          <h1 className="text-5xl text-black">Leave a Message</h1>
+        </div>
+        <div className="grid grid-cols-2 gap-y-4 gap-x-2">
+          <TextField
+            id="first-name"
+            label="First Name"
+            variant="outlined"
+            error={inputErr.first}
+            helperText={inputErrMsg.first}
+            value={firstname}
             onChange={(e) => setFirstname(e.target.value)}
           />
-        </div>
-        <div className="w-full md:w-1/2 px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-            htmlFor="grid-last-name"
-          >
-            Last Name
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
-            type="text"
-            placeholder="Last"
+          <TextField
+            id="last-name"
+            label="Last Name"
+            variant="outlined"
+            error={inputErr.last}
+            helperText={inputErrMsg.last}
+            value={lastname}
             onChange={(e) => setLastname(e.target.value)}
           />
-        </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-            htmlFor="grid-password"
-          >
-            E-mail
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          <TextField
+            className="col-span-2"
+            fullWidth
             id="email"
+            label="Email"
+            variant="outlined"
+            error={inputErr.email}
+            helperText={inputErrMsg.email}
             type="email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-        </div>
-      </div>
-      <div className="flex flex-wrap -mx-3">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-            htmlFor="grid-password"
-          >
-            Message
-          </label>
-          <textarea
-            className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+          <TextField
             id="message"
+            className="col-span-2"
+            fullWidth
+            label="Message"
+            variant="outlined"
+            multiline
+            rows={4}
+            error={inputErr.msg}
+            helperText={inputErrMsg.msg}
             onChange={(e) => setMsg(e.target.value)}
-          ></textarea>
+            value={msg}
+          />
+          <Button className="col-span-2" variant="outlined" type="submit">
+            Send
+          </Button>
         </div>
-      </div>
-      <div className="flex justify-end mb-4">
-        <button className="btn right-0" type="submit">
-          Send
-        </button>
-      </div>
-    </form>
+      </form>
+    </section>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
 import ContactForm from "../components/ContactForm";
 
+import { SnackbarProvider, VariantType, useSnackbar } from "notistack";
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -15,13 +17,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div> */}
       </div>
     </header>
   );
@@ -30,15 +25,17 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="CrossCopy Documentation"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-        <ContactForm></ContactForm>
-      </main>
-    </Layout>
+    <SnackbarProvider maxSnack={3}>
+      <Layout
+        title={`${siteConfig.title}`}
+        description="CrossCopy Documentation"
+      >
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+          <ContactForm />
+        </main>
+      </Layout>
+    </SnackbarProvider>
   );
 }
